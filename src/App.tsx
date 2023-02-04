346ae44f9b2828c82fad138e45b20d1306afd57b
jsh0116 <seunghoon7561@gmail.com>
2023-02-04 14:48:09 +0900

    [feat] 마우스 포인터 따라가기 기능
    - 마우스가 움직일때마다 커서의 현재 위치를 state에 업데이트
    - state가 업데이트 될때마다 state값에 대응해서 pointer에 top, left를 변경해준다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+type Position = {
+  x: number;
+  y: number;
+}
+
 const App = (): JSX.Element => {
-  const name = 'seunghoon';
-  const list = ['milk', 'strawberry', 'banana', 'yogurt'];
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+
+  const pointRef = useRef<HTMLDivElement>(null);
+
+  // 마우스가 움직일때 마다 커서의 현재 위치를 state에 업데이트한다 
+  document.documentElement.addEventListener('mousemove', (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    setPosition({ x: clientX, y: clientY });
+  });
+
+  // state가 업데이트 될때마다 state값에 대응해서 pointer의 top, left를 변경해준다.
+  useEffect(() => {
+    if (pointRef.current !== null) {
+      pointRef.current.style.left = position.x - 10 + 'px';
+      pointRef.current.style.top = position.y - 10 + 'px';
+    }
+  }, [position.x, position.y]);
+
   return (
-    <>
-      <h1 className='orange'>{`Hello! ${name}`}</h1>
-      <h2>Hello!</h2>
-      <p>{name}</p>
-      <ul>
-        {list.map((item) => {
-          return <li>{item}</li>
-        })}
-      </ul>
-      <img
-        style={{ width: '200px', height: '200px' }}
-        src='https://images.unsplash.com/photo-1674505613923-98085d3712fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-        alt='nature'
-      />
-    </>
+    <div className='container'>
+      <div ref={pointRef} className='pointer' />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AppProfile from './AppProfile';
-import AppCounter from './AppCounter';
-import AppProducts from './AppProducts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <App />
   </React.StrictMode>
 );
