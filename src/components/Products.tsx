8eb5bba951693819c9c9a449d24f7e3d55013344
jsh0116 <seunghoon7561@gmail.com>
2023-03-19 14:18:53 +0900

    custom hook으로 로직 재사용
    - useProducts 추가

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import useProducts from '../hooks/useProducts';
 import { Product } from '../model/Product';
 
 const Products = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>();
   const [checked, setChecked] = useState(false);
-  const [products, setProducts] = useState<Product[]>([]);
-
+  const [loading, error, products] = useProducts(checked);
   const handleChange = () => {
     setChecked((prev) => !prev);
   }
-
-  /**
-   * fetch로 받아온 데이터로 state update할 때 re-render
-   * re-render할때마다 fetch 안하도록 useEffect로 컴포넌트 마운트 시 한번만 수행하도록 수정
-   */
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-    fetch(`data/${checked ? 'sale_' : ''}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data);
-      })
-      .catch((e) => setError('error!'))
-      .finally(() => setLoading(false))
-
-
-    return () => {
-      console.log('clear');
-    }
-  }, [checked]);
 
   if (loading) return <p>Loading...</p>;
 
---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Product } from "../model/Product";
+
+type Hook = (salesOnly: boolean) => [
+  loading: boolean,
+  error: string | undefined,
+  products: Product[]
+];
+
+const useProducts: Hook = (salesOnly: boolean) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+  const [products, setProducts] = useState<Product[]>([]);
+
+  /**
+   * fetch로 받아온 데이터로 state update할 때 re-render
+   * re-render할때마다 fetch 안하도록 useEffect로 컴포넌트 마운트 시 한번만 수행하도록 수정
+   */
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((e) => setError('error!'))
+      .finally(() => setLoading(false))
+
+
+    return () => {
+      console.log('clear');
+    }
+  }, [salesOnly]);
+
+  return [loading, error, products];
+};
+
+export default useProducts;
